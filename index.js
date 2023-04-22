51f3db07a5d7986d9ff2446b637d32219bf616ca
Dany2298 <dany22986@gmail.com>
2023-04-22 13:51:17 +0100

    Modal overlay loading and success state

---
+++ index.js
@@ -0,0 +1,29 @@
+// template_a57199g
+//
+// R_WFFTv1Zl5mqZEgW
+
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += "modal__overlay--visible";
+  emailjs
+    .sendForm(
+      "service_eehx5i6",
+      "template_a57199g",
+      event.target,
+      "R_WFFTv1Zl5mqZEgW"
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += "modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable. Please contact me directly on danyfrontend@gmail.com"
+      );
+    });
+}
+
+function toggleModal() {}
