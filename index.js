5408c55a3eefc141311f36f7feda5c7e1050b647
Dany2298 <dany22986@gmail.com>
2023-04-22 16:55:24 +0100

    toggleModal function added to HTML elements

--- index.js
+++ index.js
@@ -26,4 +26,12 @@ function contact(event) {
     });
 }
 
-function toggleModal() {}
+let isModalOpen = false;
+function toggleModal() {
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
+  }
+  isModalOpen = true;
+  document.body.classList += "modal--open";
+}
